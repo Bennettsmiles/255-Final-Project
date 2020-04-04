5594c3b1d02d03e5a01df39791f9ed3f20559501
Jacob Bennett <1212smiles@gmail.com>
2020-04-03 20:08:15 -0400

    Revert "Hard Reset"
    This reverts commit c787deea8c6ef68b8c16f7ae339c897f9486f0fa.

--- src/DataBase.java
+++ src/DataBase.java
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 /**
  * @authors: Jacob Bennett
  * Keith Kwan
@@ -126,3 +127,67 @@ public class DataBase {
     }
 }
 >>>>>>> parent of d01ed53... added my name to authors section
+=======
+/**
+ * @authors: Jacob Bennett
+ * Keith Kwan
+ * Kyle Allen
+ * Eric Nguyen
+ * Kaveen Heenatigala
+ * Cole Harris
+ */
+
+import java.util.ArrayList;
+
+public class DataBase {
+    private static ArrayList<Student> studentList = new ArrayList<>();
+    private static ArrayList<Teacher> teacherList = new ArrayList<>();
+    private static ArrayList<Counselor> counselorList = new ArrayList<>();
+    
+    public static Student StudentIDLookUp(String ID){
+        for(int i = 0; i < DataBase.studentList.size(); i++){
+            if(DataBase.studentList.get(i).getID().equals(ID)){
+                return DataBase.studentList.get(i);
+            }
+        }
+        return null;
+    }
+    public static Teacher TeacherIDLookUp(String ID){
+        for(int i = 0; i < DataBase.teacherList.size(); i++){
+            if(DataBase.teacherList.get(i).getID().equals(ID)){
+                return DataBase.teacherList.get(i);
+            }
+        }
+        return null;
+    }
+    
+        public static Counselor CounselorIDLookUp(String ID){
+        for(int i = 0; i < DataBase.counselorList.size(); i++){
+            if(DataBase.counselorList.get(i).getID().equals(ID)){
+                return DataBase.counselorList.get(i);
+            }
+        }
+        return null;
+    }
+        
+    public static void AddTeacher(Teacher teacher){
+        teacherList.add(teacher);
+    }
+    public static void AddStudent(Student student){
+        studentList.add(student);
+    }
+    public static void AddCounselor(Counselor counselor){
+        counselorList.add(counselor);
+    }
+    public static ArrayList<Teacher> GetTeacherList(){
+        return teacherList;
+    }
+    public static ArrayList<Student> GetStudentList(){
+        return studentList;
+    }
+    
+    public static ArrayList<Counselor> GetCounselorList(){
+        return counselorList;
+    }
+}
+>>>>>>> parent of c787dee... Revert "added cole to authors"
--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 /**
  * @authors: Jacob Bennett
  * Keith Kwan
@@ -126,4 +127,68 @@ public abstract class Person{
         this.age = age;
     }
 >>>>>>> parent of d01ed53... added my name to authors section
+=======
+/**
+ * @authors: Jacob Bennett
+ * Keith Kwan
+ * Kyle Allen
+ * Eric Nguyen
+ * Kaveen Heenatigala
+ * Cole Harris
+ */
+
+public abstract class Person{
+    
+    private String firstName = "";
+    private String lastName = "";
+    private int age = 0;
+       
+    public Person(String first, String last, int age){
+        setFirstName(first);
+        setLastName(last);
+        setAge(age);
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    private void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    private void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    private void setAge(int age) {
+        this.age = age;
+    }
+>>>>>>> parent of c787dee... Revert "added cole to authors"
     }
