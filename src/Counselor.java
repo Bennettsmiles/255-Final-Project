9e7199fe614df371c9ac38f294995a5a468f801c
Jacob Bennett <1212smiles@gmail.com>
2020-04-03 20:04:14 -0400

    Revert "added my name to authors section"
    This reverts commit d01ed5316c2a051afce61d563f256d288650a2f7.

--- src/Counselor.java
+++ src/Counselor.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /**
  * @authors: Jacob Bennett
  * Keith Kwan
@@ -101,3 +102,109 @@ public class Counselor extends Person{
     }
     
 }
+=======
+/**
+ *
+ * @authors: Jacob Bennett
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+public class Counselor extends Person{
+    
+    private double salary = 0;
+    private int tenure = 0;
+    private int numberOfStudents = 0;
+    //private String department = "";
+    private String realcID = "";
+    private static String cID = "C0";
+    
+    public Counselor(String first, String last, int age, double salary, int tenure, int numberOfStudents){
+            super(first, last, age);
+            setSalary(salary);
+            setTenure(tenure);
+            setNumberOfStudents(numberOfStudents);
+            //setDepartment(dept);
+            setID();
+            cID = updatetID(cID);
+    }
+    
+    private static String updatetID(String id){
+        int num = Integer.valueOf(id.substring(1, id.length()));
+        num = num + 1;
+        id = "C" + Integer.toString(num);
+        return id;
+    }
+
+    /**
+     * @return the salary
+     */
+    public double getSalary() {
+        return salary;
+    }
+
+    /**
+     * @param salary the salary to set
+     */
+    private void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    /**
+     * @return the tenure
+     */
+    public int getTenure() {
+        return tenure;
+    }
+
+    /**
+     * @param tenure the tenure to set
+     */
+    private void setTenure(int tenure) {
+        this.tenure = tenure;
+    }
+
+    /**
+     * @return the department
+     */
+    /*public String getDepartment() {
+        return department;
+    }*/
+
+    /**
+     * @param department the department to set
+     */
+    /*private void setDepartment(String department) {
+        this.department = department;
+    }*/
+
+    /**
+     * @return the tID
+     */
+    public String getID() {
+        return this.realcID;
+    }
+    
+    private void setID(){
+        this.realcID = cID;
+    }
+
+    /**
+     * @return the numberOfStudents
+     */
+    public int getNumberOfStudents() {
+        return numberOfStudents;
+    }
+
+    /**
+     * @param numberOfStudents the numberOfStudents to set
+     */
+    private void setNumberOfStudents(int numberOfStudents) {
+        this.numberOfStudents = numberOfStudents;
+    }
+    
+}
+>>>>>>> parent of d01ed53... added my name to authors section
--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /**
  * @authors: Jacob Bennett
  * Keith Kwan
@@ -60,4 +61,69 @@ public abstract class Person{
     private void setAge(int age) {
         this.age = age;
     }
+=======
+/**
+ *
+ * @authors: Jacob Bennett
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+public abstract class Person{
+    
+    private String firstName = "";
+    private String lastName = "";
+    private int age = 0;
+       
+    public Person(String first, String last, int age){
+        setFirstName(first);
+        setLastName(last);
+        setAge(age);
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    private void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    private void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    private void setAge(int age) {
+        this.age = age;
+    }
+>>>>>>> parent of d01ed53... added my name to authors section
     }
